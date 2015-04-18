a7f6dc488b7ff4f9e6dc53d4bd188b2db991cd84
Nico Bischof <nico.bischof@me.com>
2015-04-18 12:38:59 +0200

    Klassenaufruf angepasst (Siehe Kommentar am Anfang der Datei) Funktion "select()" überarbeitet. Ungetestet

---
+++ core/controller/database.controller.php
@@ -0,0 +1,148 @@
+<?php
+if(!defined("HICKS_HOME")){exit;} // Sicherheitscheck
+
+/*
+ * Diese Klasse arbeitet in einer einzelnen Instanz. Sie darf nicht als neue Instanz eingelesen werden. 
+ * Benutzt wird: hicks_database::getInstance(); . Die gültige Instanz wird dann zurückgegeben.
+ */
+class hicks_database {
+	
+	private static $instance = null;
+	
+	private $type;
+	private $host;
+	private $user;
+	private $pass;
+	private $name;
+	private $prefix;
+	
+	private $uplink;
+	
+	
+	/**
+	 * Verhindert, dass die Klasse in mehreren Instanzen existiert
+	 * @return databaseInstance
+	 */
+	public static function getInstance()
+	{
+		if ( self::$instance === null ) {
+			self::$instance = new self();
+			self::$instance->intinalize(); // Wenn die Instanz zum ersten mal abgerufen wird, werden die Abhängigkeiten eingelesen
+		}
+		return self::$instance; // Gibt die Instanz der Klasse zurück
+	}
+	/**
+	 * Initialisiert den Controller
+	 */
+	public function intinalize() {
+		$this->type = hicks_config::database_type(); // Lädt sich den datenbanktypen aus der config (Derzeitig unrelevant)
+		$this->host = hicks_config::database_host(); 
+		$this->user = hicks_config::database_user();
+		$this->pass = hicks_config::database_pass();
+		$this->name = hicks_config::database_name();
+		$this->prefix = hicks_config::database_prefix();
+		
+		try{  // Fängt Fehler beim Verbindungsaufbau ab
+		$this->uplink = new PDO('mysql:host='.$this->host.';dbname='.$this->name.';charset=utf8;', $this->user, $this->pass);
+		}catch (PDOExeption $ex){
+			// ERROR 
+		}		
+		
+	}
+	/**
+	 * Datenbankabfrage | Gibt einen Array zurück 
+	 * @param string $table
+	 * @param string $column
+	 * @param string $where
+	 * @param unknown $arrayTyp
+	 * @return array
+	 */
+	public function select($table, $column = "*", $where = null, $arrayTyp = PDO::FETCH_ASSOC) {
+		$variables = array();
+		$wherequery = "";
+		if(!$where == null){
+			$wherequery .= "WHERE ";
+   			/*$where = array(
+   					
+					array("name" => "id", "value" => "1"),
+					array("name" => "id", "value" => "2")
+			);
+			*/
+   			$int = 0;
+			foreach ($where as $arg){								// Erzeugt den WHERE-Teil des Querys
+				$wherequery .= ":".$int."name = :".$int."value ";
+				$variables[":".$int."name"] = $arg['name'];
+				$variables[":".$int."value"] = $arg['value'];
+				$int++;
+			}
+		}
+		$ret = array();	
+		$variables[":column"] = $column;
+		$variables[":table"] = $this->prefix.$table;
+		$sql = "SELECT :column FROM :table".$wherequery;
+		
+		try {
+			$stmt = $this->uplink->prepare($sql);
+			$stmt->execute();
+			$result = $stm->fetchAll($arrayTyp);
+
+			//* Die erste Zeile ([0]) enthält Status-infors */
+		} catch (Exception $e) {
+			echo 'Exception -> ';
+			var_dump($e->getMessage());
+		}
+		$error = $this->uplink->errorInfo();  //  Die errorInfo() wird aus dem PDO Object und nicht aus dem PDOStatement bezogen ;)
+		$ret[] = array('count' => $stm->rowCount(), 'error' => $error[2], 'sql' => $sql);
+
+		/* associative and numeric array */
+		foreach ($result as $row) {
+			$ret[] = $row;
+		}
+
+		/* free result set */
+		$stm->closeCursor();
+
+		return $ret;
+	}
+
+	public function insert($table, $pageArray) {
+		try {
+			$sql = "INSERT INTO $this->tablepraefix$table (";
+			$values = "";
+			foreach ($pageArray as $key => $value) {
+				$sql .= $key . ", ";
+				$values .= ":" . $key . ", ";
+			}
+			$sql = substr($sql, 0, -2) . ") VALUES (";
+			$sql .= substr($values, 0, -2) . ")";
+			$statement = $this->link->prepare($sql);
+			$count = $statement->execute($pageArray);
+			return $count;
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public function update($table, $setarray, $where) {
+		try {
+			$sql = "UPDATE $this->tablepraefix$table SET ";
+			foreach ($setarray as $key => $value) {
+				$sql .= "`$key` = :$key, ";
+				$execArray[] = $key . " = " . $value;
+			}
+			$sql = substr($sql, 0, -2) . " WHERE $where";
+			$statement = $this->link->prepare($sql);
+			$count = $statement->execute($setarray);
+			return $count;
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public function delete() {
+
+	}
+
+}
+
+?>
--- core/controller_manager.php
+++ core/controller_manager.php
@@ -1,5 +1,10 @@
 <?php
 if(!defined("HICKS_HOME")){exit;} // Sicherheitscheck
 
+/*$controller_list = array(
+	array("name" => "Database Controller","file" => HICKS_HOME."/core/controller/database.controller.php"),
+	array("name" => "Session Manager","file" => HICKS_HOME."/core/controller/session.controller.php")
+);
+*/
 
 ?>
