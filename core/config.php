e0fb75864585d46a530e41ceb22f5d077cdbd02e
Nico Bischof <nico.bischof@me.com>
2015-04-18 11:54:45 +0200

    Konfigurationsdatei angelegt

---
+++ core/config.php
@@ -0,0 +1,50 @@
+<?php
+if(!defined("HICKS_HOME")){exit;} // Sicherheitscheck
+
+class hicks_config{
+
+/**
+ * Gibt den Datenbanktyp zurück
+ * @return string
+ */
+public static function database_type(){
+	return "mysql_pdo";
+}
+/**
+ * Gibt den Datenbankhost zurück
+ * @return string
+ */
+public static function database_host(){
+	return "localhost";
+}
+/**
+ * Gibt den Datenbankuser zurück
+ * @return string
+ */
+public static function database_user(){
+	return "";
+}
+/**
+ * Gibt das Datenbankpasswort zurück
+ * @return string
+ */
+public static function database_pass(){
+	return "";
+}
+/**
+ * Gibt den Datenbanknamen zurück
+ * @return string
+ */
+public static function database_name(){
+	return "";
+}
+/**
+ * Gibt den Tabellenprefix für die Datenbank zurück
+ * @return string
+ */
+public static function database_prefix(){
+	return "";
+}
+
+}
+?>
