875b396d84bbbe2b367161efe0bdeb3506dba40b
weltsieger <sieger.welt@gmail.com>
2015-04-21 16:52:46 +0200

    Controller aufgeräumt
    request_manager in request.controller umbenannt und in /core/controller
    verschoben
    controller_manager in core.php verschoben

---
+++ core/controller/request.controller.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined("HICKS_HOME")) {
+    exit;
+} // Sicherheitscheck
+
+?>
--- core/core.php
+++ core/core.php
@@ -4,4 +4,10 @@ if (!defined("HICKS_HOME")) {
     exit;
 } // Sicherheitscheck
 
+/* $controller_list = array(
+  array("name" => "Database Controller","file" => HICKS_HOME."/core/controller/database.controller.php"),
+  array("name" => "Session Manager","file" => HICKS_HOME."/core/controller/session.controller.php")
+  );
+ */
+
 ?>
