b11e6ca06d70094ae2b3bb6221764df65ae0dcb4
Nico Bischof <nico.bischof@me.com>
2015-04-25 04:18:00 +0200

    Headermodule soweit fertig. Evtl. muss das Modul angepasst werden.

--- core/modules/header.module.php
+++ core/modules/header.module.php
@@ -3,7 +3,24 @@ if (!defined("HICKS_HOME")) {
 	exit;
 } // Sicherheitscheck
 
+
 $out = hicks_output::getInstance();
-$out->push("");
+$out->push('<!DOCTYPE html>
+		<html lang="de">
+		<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">	
+		');
+$out->push('<title>Pony Express - German Tail</title>');  // Titel
+$out->push('<link href="http://cdn.livespawn.de/static/bootstrap/3_3_4/bootstrap.css" rel="stylesheet">
+		<link href="static/style.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+		</head>
+		<body>');
+
 
 ?>
