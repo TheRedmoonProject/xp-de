f3657c2b986c1f831550fba43afd61965c20a520
weltsieger <sieger.welt@gmail.com>
2015-04-26 09:57:47 +0200

    Session-Controller angefangen

--- core/controller/session.controller.php
+++ core/controller/session.controller.php
@@ -1,12 +1,26 @@
 <?php
+
 if (!defined("HICKS_HOME")) {
     exit;
 } // Sicherheitscheck
 
 class hicks_session {
-	
-	
-	
-	
+    /* ToDo:
+     * - neue SESSION-Instanz erkennen + anlegen... done
+     * - Ein- und Ausgabe von SESSION var in / aus DB
+     * - Cookies definieren - dauerhaft
+     */
+
+    private static $instance = null;
+    private $phpSessionId;
+
+    public function __construct() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance; // Gibt die Instanz der Klasse zurück
+    }
+
 }
+
 ?>
