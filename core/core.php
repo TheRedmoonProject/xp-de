e8f0bf9555136a38fab0dd3505e88aac7876192e
weltsieger <sieger.welt@gmail.com>
2015-04-26 11:09:57 +0200

    SQL-Statements in Beispiel zusammengefasst

--- core/core.php
+++ core/core.php
@@ -13,7 +13,7 @@ if (!defined("HICKS_HOME")) {
 // Alle Controller einlesen
 require_once 'controller/error.controller.php';
 require_once 'controller/database.controller.php';
-require_once 'controller/output.controller.php';
+ require_once 'controller/output.controller.php';
 
 $db = hicks_database::getInstance(); // Initialisiert die Datenbank
 
@@ -21,30 +21,39 @@ $page_name = $_GET['page_name'];
 $param = $_GET['param'];
 
 $lang = 'de-DE';
-
-
-
-
-$db = new hicks_database();
-$result = $db->select('page','`name`,`content`,`page_type`',array(array("name" => "name", "value" => $page_name),array("name" => "lang", "value" => $lang)));
-
-if($page_name == ''){
-	$page_name = 'start';
+if ($page_name == '') {
+    $page_name = 'start';
 }
 
-if($result['name'] == $page_name || $result['name'] == 'start'){
-	
-	$page_type_stmt = $db->select('page_type','`id`,`name`',array(array("name" => "id", "value" => $result['type'])));
-	if($page_type_stmt['id'] == $result['id']){
-		require_once HICKS_HOME.'/modules/'.$module.'/index.php';
-	}else{
-		// Error Page Type nicht gefunden
-	}
-		
-		
-	
-}else{
-	// ERROR  (Seite existiert nicht)
+/*
+ * SELECT p.id, p.name, p.intermal_name, p.content, p.type, p.lang, pt.id, pt.name, pt.active, pt.beta 
+ * FROM page as p, page_type as pt 
+ * WHERE p.id = pt.id 
+ * AND page.name = '$page_name'
+ * 
+ */
+
+$sql = "SELECT p.id, p.name, p.intermal_name, p.content, p.type, p.lang, pt.id, pt.name, pt.active, pt.beta 
+FROM page as p, page_type as pt 
+WHERE p.id = pt.id 
+AND page.name = '$page_name'";
+
+$db = new hicks_database();
+$result = $db->select('page', '`name`,`content`,`page_type`', 
+        array(array("name" => "name", "value" => $page_name), 
+            array("name" => "lang", "value" => $lang)));
+
+
+if ($result['name'] == $page_name || $result['name'] == 'start') {
+
+    $page_type_stmt = $db->select('page_type', '`id`,`name`', array(array("name" => "id", "value" => $result['type'])));
+    if ($page_type_stmt['id'] == $result['id']) {
+        require_once HICKS_HOME . '/modules/' . $module . '/index.php';
+    } else {
+        // Error Page Type nicht gefunden
+    }
+} else {
+    // ERROR  (Seite existiert nicht)
 }
 
 $output = hicks_output::getInstance();
